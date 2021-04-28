314c10beb884a462fcc9e24c684974b621114010
Gabriel Albuquerque da Rosa <gabaoalb@gmail.com>
2021-04-27 21:23:59 -0300

    lesson 79

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './transactions.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,6 +18,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final List<Transaction> transactions = [
+    Transaction(id: '0', title: 'Tênis novos', amount: 299.99, date: DateTime.now()),
+    Transaction(id: '1', title: 'Compras da semana', amount: 16.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,19 +31,44 @@ class MyHomePage extends StatelessWidget {
         title: Text('App de Gastos'),
       ),
       body: Column(
-        children: [
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
           Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.purple,
-                child: Text(
-                  'Chart',
-                  textAlign: TextAlign.center,
-                ),
-              )),
-          Card(
-            child: Text('List of TX'),
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
           ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2,)),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      )
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Text(tx.title),
+                      Text(tx.date.toString())
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
         ],
       ),
     );
