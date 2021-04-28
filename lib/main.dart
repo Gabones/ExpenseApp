cb4d0567c00140b5c11d283ae6f0f1ec90c903ec
Gabriel Albuquerque da Rosa <gabaoalb@hotmail.com>
2021-04-28 13:18:50 -0300

    lesson 86

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transactions.dart';
 
 void main() => runApp(MyApp());
@@ -23,6 +24,10 @@ class MyHomePage extends StatelessWidget {
     Transaction(id: '0', title: 'Tênis novos', amount: 299.99, date: DateTime.now()),
     Transaction(id: '1', title: 'Compras da semana', amount: 16.99, date: DateTime.now())
   ];
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +36,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('App de Gastos'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -40,6 +45,32 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('CHART!'),
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    // onChanged: (value) => titleInput = value,
+                    controller: titleController,
+                    decoration: InputDecoration(labelText: 'Titulo'),
+                  ),
+                  TextField(
+                    // onChanged: (value) => amountInput = value,
+                    controller: amountController,
+                    decoration: InputDecoration(labelText: 'Total'),
+                  ),
+                  TextButton(
+                    child: Text('Add Compra'),
+                    onPressed: () => null,
+                    style: ButtonStyle(foregroundColor: MaterialStateProperty.all(Colors.purple)),
+                  )),
+                ],
+              ),
             ),
           ),
           Column(
@@ -70,7 +101,7 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       Text(
-                        tx.date.toString(),
+                        DateFormat('dd/MM/yyyy HH:mm').format(tx.date),
                         style: TextStyle(
                           fontSize: 14,
                           color: Colors.grey,
