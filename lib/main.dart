6adaef46ab06f1e0798655aac11a8ba03e8f4a9a
Gabriel Albuquerque da Rosa <gabaoalb@gmail.com>
2021-06-06 20:59:02 -0300

    final module

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
@@ -40,13 +40,30 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Transaction> _userTransactions = [
     // Transaction(id: '0', title: 'Tênis novos', amount: 299.99, date: DateTime.now()),
     // Transaction(id: '1', title: 'Compras da semana', amount: 16.99, date: DateTime.now())
   ];
 
   bool _showChart = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
@@ -84,26 +101,75 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  List<Widget> _buildLandscapeContent(
+      MediaQueryData mediaQuery, AppBar appBar, Widget txListWidget) {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Show Chart'),
+          Switch.adaptive(
+              activeColor: Theme.of(context).accentColor,
+              value: _showChart,
+              onChanged: (val) {
+                setState(() {
+                  _showChart = val;
+                });
+              }),
+        ],
+      ),
+      _showChart
+          ? Container(
+              height: (mediaQuery.size.height -
+                      appBar.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.7,
+              child: Chart(_recentTransactions),
+            )
+          : txListWidget
+    ];
+  }
+
+  List<Widget> _buildPortraitContent(
+      MediaQueryData mediaQuery, AppBar appBar, Widget txListWidget) {
+    return [
+      Container(
+        height: (mediaQuery.size.height -
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
+            0.3,
+        child: Chart(_recentTransactions),
+      ),
+      txListWidget
+    ];
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return Platform.isAndroid
+        ? AppBar(
+            title: Text('App de Gastos'),
+            actions: [
+              IconButton(
+                icon: Icon(CupertinoIcons.add),
+                onPressed: () => _startAddNewTransaction(context),
+              )
+            ],
+          )
+        : CupertinoNavigationBar(
+            middle: Text('App de Gastos'),
+            trailing: IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+            ),
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('My HomePage Build');
     final isLandscape =
         MediaQuery.of(context).orientation == Orientation.landscape;
-    final PreferredSizeWidget appBar = Platform.isAndroid ? AppBar(
-      title: Text('App de Gastos'),
-      actions: [
-        IconButton(
-          icon: Icon(CupertinoIcons.add),
-          onPressed: () => _startAddNewTransaction(context),
-        )
-      ],
-    )
-    : CupertinoNavigationBar(
-      middle: Text('App de Gastos'),
-      trailing: IconButton(
-        icon: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
-    );
+    final PreferredSizeWidget appBar = _buildAppBar();
     final txListWidget = Container(
       height: (MediaQuery.of(context).size.height -
               appBar.preferredSize.height -
@@ -112,60 +178,33 @@ class _MyHomePageState extends State<MyHomePage> {
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
     final mediaQuery = MediaQuery.of(context);
-    final pageBody = SafeArea(child: SingleChildScrollView(
+    final pageBody = SafeArea(
+        child: SingleChildScrollView(
       child: Column(
         // mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           if (isLandscape)
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Show Chart'),
-                Switch.adaptive(
-                    activeColor: Theme.of(context).accentColor,
-                    value: _showChart,
-                    onChanged: (val) {
-                      setState(() {
-                        _showChart = val;
-                      });
-                    }),
-              ],
-            ),
+            ..._buildLandscapeContent(mediaQuery, appBar, txListWidget),
           if (!isLandscape)
-            Container(
-              height: (mediaQuery.size.height -
-                  appBar.preferredSize.height -
-                  mediaQuery.padding.top) *
-                  0.3,
-              child: Chart(_recentTransactions),
-            ),
-          if (!isLandscape) txListWidget,
-          if (isLandscape)
-            _showChart
-                ? Container(
-              height: (mediaQuery.size.height -
-                  appBar.preferredSize.height -
-                  mediaQuery.padding.top) *
-                  0.7,
-              child: Chart(_recentTransactions),
-            )
-                : txListWidget
+            ..._buildPortraitContent(mediaQuery, appBar, txListWidget),
         ],
       ),
     ));
-    return Platform.isAndroid ? Scaffold(
-      appBar: appBar,
-      body: pageBody,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Platform.isIOS ?
-      Container()
-      : FloatingActionButton(
-        child: Icon(Icons.add),
-        foregroundColor: Colors.white,
-        onPressed: () => _startAddNewTransaction(context),
-      ),
-    )
-    : CupertinoPageScaffold(child: pageBody);
+    return Platform.isAndroid
+        ? Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    foregroundColor: Colors.white,
+                    onPressed: () => _startAddNewTransaction(context),
+                  ),
+          )
+        : CupertinoPageScaffold(child: pageBody);
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,6 +1,6 @@
 import '../models/transactions.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import '../widgets/transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -10,71 +10,38 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('Build for transaction List()');
     return Container(
-      height: MediaQuery.of(context).size.height * 0.6,
-      child: transactions.isEmpty
-          ? LayoutBuilder(builder: (ctx,constraints) {
-            return Column(
-              children: [
-                Text(
-                  'No transactions !?',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Container(
-                  height: constraints.maxHeight * 0.6,
-                  child: Image.asset(
-                    'assets/image/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ],
-            );
-      })
-          : ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6.0),
-                        child: FittedBox(
-                            child: Text('\$${transactions[index].amount}')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
+        height: MediaQuery.of(context).size.height * 0.6,
+        child: transactions.isEmpty
+            ? LayoutBuilder(builder: (ctx, constraints) {
+                return Column(
+                  children: [
+                    Text(
+                      'No transactions !?',
                       style: Theme.of(context).textTheme.headline6,
                     ),
-                    subtitle:
-                        Text(DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: MediaQuery.of(context).size.width > 460 ?
-                    TextButton.icon(
-                     label: Text('Delete'),
-                     icon: Icon(Icons.delete),
-                     onPressed: () => deleteTx(transactions[index].id),
-                      style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(Theme.of(context).errorColor),
-                      ),
-                    )
-                    : IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () => deleteTx(transactions[index].id),
+                    SizedBox(
+                      height: 15,
                     ),
-                  ),
+                    Container(
+                      height: constraints.maxHeight * 0.6,
+                      child: Image.asset(
+                        'assets/image/waiting.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
                 );
-              },
-              itemCount: transactions.length,
-            ),
-    );
+              })
+            : ListView(
+                children: transactions
+                    .map((tx) => TransactionItem(
+                          key: ValueKey(tx.id),
+                          transaction: tx,
+                          deleteTx: deleteTx,
+                        ))
+                    .toList(),
+              ));
   }
 }
